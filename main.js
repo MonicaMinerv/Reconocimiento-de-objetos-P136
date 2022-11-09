cf5cd8efc28364d1184c05e4cd770402c4edf37d
MonicaMinerv <106777034+MonicaMinerv@users.noreply.github.com>
2022-11-09 11:22:56 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+status = "";
+video = "";
+
+
+function setup() {
+  canvas = createCanvas(480, 380);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+}
+
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Estado: Detectando objetos";
+  object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded() {
+  console.log("¡Modelo cargado!");
+  status = true;
+ }
+
+
+function draw() {
+  image(video, 0, 0, 480, 380);
+}
